f12040ebfb4dddf2411ef1d0f034f94fbc847e1e
Aviroop Nandy <60533497+AviroopNandy@users.noreply.github.com>
2020-12-26 18:29:44 +0530

    update admin files

--- src/containers/Admin/Admin.jsx
+++ src/containers/Admin/Admin.jsx
@@ -1,5 +1,4 @@
 import React,{ useState } from "react";
-import { Link } from "react-router-dom";
 import Background from '../../hoc/Background/Background';
 import TechQuestions from '../../components/AdminQuestions/Questions/TechnicalQuestions';
 import DesignQuestions from '../../components/AdminQuestions/Questions/DesignQuestions';
@@ -8,7 +7,7 @@ import Logout from '../Logout/Logout';
 import classes from './Admin.module.css';
 import { Redirect } from "react-router-dom";
 
-const Admin = (props)=>{
+const Admin = (props) => {
     const [checkVal,setCheckVal]=useState("");
     function getSelectedVal(event){
         setCheckVal(event.target.value);
@@ -20,14 +19,53 @@ const Admin = (props)=>{
             <Redirect to="/login" />
         )
     }
+
+    let statusValue = false;
+
+    function setStatus(event) {
+        statusValue = event.target.value;
+    }
+
+    function updateStatus() {
+        // console.log("Status: ", statusValue);
+        fetch("http://adgrecruitments.herokuapp.com/admin/statusupdate?recruitment=" + statusValue, {
+            method: "GET",
+            headers: {
+                "auth-token": sessionStorage.getItem("admin"),
+            },
+        })
+        .then((response) => {
+            console.log(response);
+            return response.json();
+        })
+        .then((data) => {
+            // console.log(data);
+        })
+        .catch(error => {
+            // console.log(error)
+            alert("Error: ", error);
+        })
+    }
     
     return(
     <Background>
         <div>
-        <div className={classes.userContainer}>
-            <Link className={ classes.userInfo } to="/leaderboard">Leaderboard</Link>
-            <Logout />
+            <div>
+                <input type="radio" name="status" value="true" onChange={ setStatus } />
+                <label htmlFor="true">On</label>
+                <input type="radio" name="status" value="false" onChange={ setStatus } />
+                <label htmlFor="false">Off</label>
+                <button onClick={ updateStatus }>Confirm!</button>
             </div>
+            {statusValue ?
+                <div>
+                    Recruitment status is currently turned ON
+                </div> :
+                <div>
+                    Recruitment status is currently turned OFF
+                </div>
+            }
+            <Logout />
         <div className={classes.heading}>
             <h1>Administrator Portal</h1>
             <p>Create, Edit and Manage Domain Specific Quizzes and Questions</p>
@@ -47,5 +85,6 @@ const Admin = (props)=>{
        <MgmtQuestions selectedValue={checkVal} />
        </div>
     </Background>
-    )};
+)};
+
 export default Admin;
